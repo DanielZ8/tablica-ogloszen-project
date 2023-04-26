1c8dabcbef7634a7b0b922fbeaae2775586a256b
DanielZ8 <danielziol@proton.me>
2023-04-27 00:21:26 +0200

    new factory data

--- database/factories/OgloszeniaFactory.php
+++ database/factories/OgloszeniaFactory.php
@@ -17,12 +17,56 @@ class OgloszeniaFactory extends Factory
     
     public function definition(): array
     {
+        static $kat = ['Python', 'Java', 'CPP', 'PHP'];
+        static $miasta = [
+            'Warszawa', 'Kraków', 'Wrocław', 
+            'Poznań', 'Białystok', 'Bielsko-Biała', 
+            'Bydgoszcz', 'Częstochowa', 'Kielce', 
+            'Rzeszów', 'Łódź', 'Toruń', 'Zielona Góra',
+            'Lublin', 'Opole', 'Gdańsk', 'Szczecin', 'ONLINE'];
+        static $naglowek = [
+            'Engineering Lead', '.NET Developer with Oracle DB', 
+            'Software Engineer (m/w/d) Web- and App-Development', 'Mid Web Developer', 
+            'Software Engineer (m/w/d) Transport Management System Road', 'Digital Analyst', 
+            'Senior React Developer', 'Manager ds. IT', 'MacOS Developer (C++)', 
+            'C++ Developer (PC Browser Engine Team)', 'Android Developer', 'Azure Cloud Expert', 
+            'Senior Embedded C++ Engineer for Computer Vision (Robotics)', 
+            'Associate Director, Specialist Programmer – Clinical Data Insight', 
+            'Specialist Programmer, Clinical Data and Insights', 'Software Quality Assurance Engineer', 
+            'Test & Validation Engineer', 'ServiceNow Front-end/UI Developer', 
+            'C# Azure Developer', 'Fullstack Java Developer', 'Senior UX Designer / Software Developer', 
+            'Lead Full-stack Engineer - JAMstack/Website', 'Senior Android Software Engineer', 
+            'Inżynier oprogramowania (język C/C++)', 'Programista (systemy wbudowane)', 
+            'Engineering Lead', '.NET Developer with Oracle DB', 'Software Engineer (m/w/d) Web- and App-Development', 
+            'Mid Web Developer', 'Software Engineer (m/w/d) Transport Management System Road', 'Digital Analyst', 
+            'Senior React Developer', 'Manager ds. IT', 'MacOS Developer (C++)', 
+            'C++ Developer (PC Browser Engine Team)', 'Android Developer', 'Azure Cloud Expert', 
+            'Senior Embedded C++ Engineer for Computer Vision (Robotics)', 
+            'Associate Director, Specialist Programmer – Clinical Data Insight', 
+            'Specialist Programmer, Clinical Data and Insights', 'Software Quality Assurance Engineer', 
+            'Test & Validation Engineer', 'ServiceNow Front-end/UI Developer', 'C# Azure Developer', 
+            'Fullstack Java Developer', 'Senior UX Designer / Software Developer', 
+            'Lead Full-stack Engineer - JAMstack/Website', 'Senior Android Software Engineer', 
+            'Inżynier oprogramowania (język C/C++)', 'Programista (systemy wbudowane)', 
+            'Programista PHP / PHP Developer', 'Inżynier Aplikacji POS (K/M)', 'Embedded Cyber Security Engineer', 
+            'Analityk Biznesowo-Systemowy', 'Programista C/C++ systemu Linux', '.NET Developer', 
+            'Senior React Frontend Developer', 'Service Desk Consultant', 
+            'DRAM Engineer with C – Automotive', 'GPU Engineer – Automotive', 'WordPress Technical Specialist', 
+            'C++ Developer', 'Fullstack Developer (Java+Angular)', 
+            'Internship - Natural Language Processing Intern in Artificial Intelligence Team', 
+            'Software Project Manager', 'Programista Fullstack', 'Senior Frontend Developer', 
+            'Working Student - Software Engineer C/C++', 
+            'Płatny Staż w Jednym z Zespołów Programistów RnD, Digitalizacji lub IT (k/m)', 
+            'SalesForce Developer', 'Embedded Software Developer', 'Technical Support Specialist', 
+            'C++ Developer', 'Power Software Engineer', 'Front-end Developer'];
+        static $stawka = [5000, 8500, 9000, 1000, 10500, 7000, 7250, 15000, 30000, 27000, 5500, 6600, 8300, 2600];
         static $number = 1;
+        
         return [
-            'naglowek' => fake()->jobTitle(),
-            'kategoria' => 'Python',
-            'stawka' => fake()->numberBetween(6000, 15000),
-            'lokalizacja' => fake()->city(),
+            'naglowek' => $naglowek[fake()->numberBetween(0,74)],
+            'kategoria' => $kat[fake()->numberBetween(0,3)],
+            'stawka' => $stawka[fake()->numberBetween(0,13)],
+            'lokalizacja' => $miasta[fake()->numberBetween(0,17)],
             'wymagania' => 'wymagania',
             'opis' => fake()->catchPhrase() ,
             'status' => 'aktualne',
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,17 +17,31 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        static $firmy = [
+            'Consult Red', 'Commerzbank AG', 
+            'DACHSER GmbH & Co. KG ', 'NetArch', 'IBA POLAND sp. z o.o.', 
+            'Salve Medica Sp. z o.o. Sp. k.', 'Opera Software International AS ', 
+            'SKILLS MATTER', 'VSTORM sp. z o.o.', 'Robert Bosch Sp. z o.o.', 
+            'AstraZeneca Pharma', 'Cyclad', 'Diehl Controls Polska', 'Ework Group',
+            'G-CORE INNOVATIONS SOCIÉTÉ A RESPONSABILITÉ LIMITÉE ', 'XPERI Poland', 'ZF Group', 
+            'Cyfrowy Polsat S.A.', 'Centrum Rozwoju Szkół Wyższych TEB Akademia', 'Fiserv Polska S.A. ', 
+            'BCF Software Sp. z o.o.', 'IT CONNECT Sp. z o.o. Sp.k.', 
+            'Prime Engineering Poland', 'Leroy Merlin Polska Sp. z o.o.', 'e-Xim IT S.A.', 
+            'MSD Poland', 'apreel sp. z o.o.', 'Samsung R&D Institute Poland', 'Signal Group sp. z o.o. sp.k.', 'KOTRAK S.A.', 
+            'NOVACURA POLAND sp. z o.o.', 'Nokia', 'TRUMPF Huettinger Sp. z o.o.', 'Dolfi 1920 Sp. z. o.o.', 
+            'Sieć Badawcza Łukasiewicz – Instytut Lotnictwa', '6CONNEX INTERNATIONAL', 'ITFS sp. z o.o.', 
+            'Antal IT Sales/ Tech', 'Cloud Services Sp. z o.o.'];
         return [
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'photo' => fake()->imageUrl(200, 200),
-            'rola' => 'company',
+            'photo' => fake()->imageUrl(400, 400),
+            'rola' => 'firma',
             'imie' => fake() -> Firstname(),
             'nazwisko' => fake() ->lastName(),
             'wiek' => fake() -> numberBetween(22, 60),
             'opis' => fake() -> sentence(),
-            'nazwa_firmy' => fake() -> company(), 
+            'nazwa_firmy' => $firmy[fake()->numberBetween(0,38)], 
             'remember_token' => Str::random(10),
         ];
     }
